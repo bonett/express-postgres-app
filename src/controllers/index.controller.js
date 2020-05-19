1a178bcbd1586dce37cad44dfa5cb431c9e996f3
Wilfrido Bonett <wbonett10@gmail.com>
2020-05-18 22:32:26 -0500

    createuser is working

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -9,11 +9,34 @@ const pool = new Pool({
 })
 
 const getUsers = async (req, res) => {
-    const response = await pool.query('SELECT * FROM users');
-    console.log(response.rows);
-    res.send('users');
+    const response = await pool.query(`SELECT * FROM users`);
+
+    try {
+        console.log(response.rows);
+        res.status(200).json(response.rows);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 }
 
+const createUser = async (req, res) => {
+    const { name, email, country } = req.body;
+    pool.query(`INSERT INTO users (name, email, country) VALUES ($1, $2, $3)`, [name, email, country]);
+
+    try {
+        res.status(201).json({
+            message: "User created succesfully",
+            body: {
+                user: { name, email, country }
+            }
+        });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+
+    res.send(`User ${name} created`);
+}
 module.exports = {
-    getUsers
+    getUsers,
+    createUser
 }
